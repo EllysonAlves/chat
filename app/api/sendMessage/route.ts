f24e47d3701d510f922cc216396555894a376781
Ellyson <ellyson.alves.ds@gmail.com>
2025-08-18 23:06:34 -0300

    fix

--- app/api/sendMessage/route.ts
+++ app/api/sendMessage/route.ts
@@ -2,29 +2,29 @@ import { NextResponse } from "next/server";
 import axios from "axios";
 
 export async function POST(req: Request) {
-  const { to, message } = await req.json();
+    const { to, message } = await req.json();
 
-  try {
-    const r = await axios.post(
-      `https://graph.facebook.com/v22.0/718972057972485/messages`,
-      {
-        messaging_product: "whatsapp",
-        to,
-        text: { body: message }
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`,
-          "Content-Type": "application/json"
-        }
-      }
-    );
+    try {
+        const r = await axios.post(
+            `https://graph.facebook.com/v22.0/718972057972485/messages`,
+            {
+                messaging_product: "whatsapp",
+                to,
+                text: { body: message }
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
 
-    return NextResponse.json({ success: true, data: r.data });
-  } catch (err: any) {
-    return NextResponse.json(
-      { error: err.response?.data || err.message },
-      { status: 500 }
-    );
-  }
+        return NextResponse.json({ success: true, data: r.data });
+    } catch (err: unknown) {
+        let message = "Erro desconhecido";
+        if (err instanceof Error) message = err.message;
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+
 }
